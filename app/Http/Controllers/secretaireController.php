86409f7f9d8601d2669994bf01f4d5b6d41b0ea8
Cheikh <ccheikh1722@gmail.com>
2025-05-24 23:19:50 +0000

    laster

--- app/Http/Controllers/secretaireController.php
+++ app/Http/Controllers/secretaireController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Consultation;
-use App\Models\User;
 
 class SecretaireController extends Controller
 {
@@ -15,53 +14,5 @@ class SecretaireController extends Controller
             ->get();
 
         return view('pages.front-end.Secretaire.DashboardSecretaire', compact('consultations'));
-    }
-
-    public function index()
-    {
-        $secretaires = User::where('profil', 'secretaire')->get();
-        return view('pages.front-end.Secretaire.index', compact('secretaires'));
-    }
-
-    public function show($id)
-    {
-        $secretaire = User::findOrFail($id);
-        return view('pages.front-end.Secretaire.show', compact('secretaire'));
-    }
-
-    public function edit($id)
-    {
-        $secretaire = User::findOrFail($id);
-        return view('pages.front-end.Secretaire.edit', compact('secretaire'));
-    }
-
-    public function update(Request $request, $id)
-    {
-        $secretaire = User::findOrFail($id);
-
-        $validatedData = $request->validate([
-            'nom' => 'required|string|max:100',
-            'prenom' => 'required|string|max:100',
-            'email' => 'required|string|email|max:150|unique:users,email,' . $secretaire->id,
-            'adresse' => 'required|string|max:255',
-            'telephone' => 'required|string|max:15',
-        ]);
-
-        $secretaire->nom = $validatedData['nom'];
-        $secretaire->prenom = $validatedData['prenom'];
-        $secretaire->email = $validatedData['email'];
-        $secretaire->adresse = $validatedData['adresse'];
-        $secretaire->telephone = $validatedData['telephone'];
-        $secretaire->save();
-
-        return redirect()->route('secretaire.index')->with('success', 'Secrétaire mis à jour avec succès.');
-    }
-
-    public function destroy($id)
-    {
-        $secretaire = User::findOrFail($id);
-        $secretaire->delete();
-
-        return redirect()->route('secretaire.index')->with('success', 'Secrétaire supprimé avec succès.');
     }
 }
